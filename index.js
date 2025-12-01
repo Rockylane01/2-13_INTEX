c3cff411e03be8f5ffd5ce88222b914c2a2dc879
Rockylane01 <rockylane01@gmail.com>
2025-12-01 15:58:54 -0700

    changing some file stucture

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 // Set up EJS as the template engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "src", "veiws"));
+app.set("views", path.join(__dirname, "src", "views"));
 
 // Middleware
 app.use(express.json());
